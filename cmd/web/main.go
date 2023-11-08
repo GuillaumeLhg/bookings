ce6f3283c644bc5ccd1cb30825715d56391c33dd
GuillaumeLhg <glhelgoualch@gmail.com>
2023-11-08 16:39:02 +0100

    Refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,9 @@ import (
 	"time"
 
 	"github.com/alexedwards/scs/v2"
-	"github.com/GuillaumeLhg/bookings/pkg/config"
-	"github.com/GuillaumeLhg/bookings/pkg/handlers"
-	"github.com/GuillaumeLhg/bookings/pkg/render"
+	"github.com/GuillaumeLhg/bookings/internal/config"
+	"github.com/GuillaumeLhg/bookings/internal/handlers"
+	"github.com/GuillaumeLhg/bookings/internal/render"
 )
 
 const portNumber = ":8080"
--- internal/render/render.go
+++ internal/render/render.go
@@ -7,8 +7,9 @@ import (
 	"net/http"
 	"path/filepath"
 
-	"github.com/GuillaumeLhg/bookings/pkg/config"
-	"github.com/GuillaumeLhg/bookings/pkg/models"
+	"github.com/GuillaumeLhg/bookings/internal/config"
+	"github.com/GuillaumeLhg/bookings/internal/models"
+	"github.com/justinas/nosurf"
 )
 
 var app *config.AppConfig
@@ -18,13 +19,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AppDefaultData(td *models.TemplateData) *models.TemplateData {
-
+func AppDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 	if app.UseCache {
 		// get the cache from the app config
@@ -41,7 +42,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AppDefaultData(td)
+	td = AppDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
